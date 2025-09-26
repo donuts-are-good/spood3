5a162fab1e6ac57fb77e77e0e3add7d3bca312b8
donuts-are-good <96031819+donuts-are-good@users.noreply.github.com>
2025-09-25 20:16:40 -0500

    adding champions

---
+++ static/js/champions.js
@@ -0,0 +1,8 @@
+(function(){
+  // Simple client-side niceties; server renders most data
+  const grid = document.querySelector('.champions-grid');
+  if (!grid) return;
+  // Future: add filters/sorting without reloading
+})();
+
+
